a3470a8de1735a76dcdd41bbd662e8b3c2fc4e47
Zach <50152024+VanVelZ@users.noreply.github.com>
2021-05-26 16:16:35 -0500

    Fixed date and added total_weeks

--- PythonAPI/models/batch.py
+++ PythonAPI/models/batch.py
@@ -18,8 +18,10 @@ class Batch:
         self.end_date = end_date
 
     def current_week(self):
-        return floor(abs((self.end_date - self.start_date).days / 7))
+        return floor(abs((datetime.now() - self.start_date).days / 7))
 
+    def total_weeks(self):
+        return floor(abs((self.end_date - self.start_date).days / 7))
 
 class TestBatch(unittest.TestCase):
 
