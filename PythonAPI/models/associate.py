5a0ca0c954d6ac097ad2fd99ce21371ebe648388
Zach <50152024+VanVelZ@users.noreply.github.com>
2021-05-25 21:09:34 -0500

    fixed init

--- PythonAPI/models/associate.py
+++ PythonAPI/models/associate.py
@@ -1,8 +1,7 @@
 
 class Associate:
 
-    def init(self, first_name, last_name, email):
+    def __init__(self, first_name, last_name, email):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-        
