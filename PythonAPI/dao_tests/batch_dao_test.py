8bcd0e6a3c5f21b3a075b94fc6c391e64fef0674
serenesamb <serenesamb@gmail.com>
2021-05-27 12:52:30 -0400

    Daos for get_all batches_by_year() is done and the unittest for it works as well

--- PythonAPI/dao_tests/batch_dao_test.py
+++ PythonAPI/dao_tests/batch_dao_test.py
@@ -1,10 +1,13 @@
 import unittest
 
+from daos import batch_dao
+from daos.batch_dao import BatchDAO
 from daos.batch_dao_impl import BatchDAOImpl
+from models.batch_model import Batch
 
 
 class DAOTests(unittest.TestCase):
-    # Batch DAO
     def test_get_all_batches_by_year(self):
-        batches = BatchDAOImpl().get_all_batches_by_year()
-        self.assertEqual(type(batches), type(None))
+        batches = BatchDAOImpl().get_all_batches_by_year('2020')
+        self.assertTrue(batches)
+
--- PythonAPI/daos/batch_dao_impl.py
+++ PythonAPI/daos/batch_dao_impl.py
@@ -4,37 +4,18 @@ from psycopg2._psycopg import connection
 
 from daos.batch_dao import BatchDAO
 from models.batch_model import Batch
+from utils.db_connection import DbConn
 
 
 class BatchDAOImpl(BatchDAO):
-    @classmethod
     def get_all_batches_by_year(self, year):
-        sql = "SELECT * FROM batches WHERE start_date = %s"
-        cursor = connection.cursor()
-        cursor.execute(sql, [year])
-        record = cursor.fetchone()
-        print(record[0])
-        if record:
-            return Batch(record[0], record[1], record[2], record[3], record[4])
-        else:
-            return f"Batch with {year} - Not Found", 404
+
+        sql = "SELECT * FROM batches WHERE date_part('year', batches.start_date) = %s "
+        record = DbConn.make_connect(sql, [year])
+        batches = []
+        for batch in record:
+            batches.append(Batch(batch[0], batch[1], batch[2], batch[3], batch[4]))
+        return batches
 
 
-    @classmethod
-    def add_batch(cls, batch_id):
-        try:
-            batches = BatchDAO.get_all_batches_by_year(batch_id)
-            batch_list = []
-            for batch in batches:
-                batch_list.append((batch.id, batch.name, batch.training_track, batch.start_date, batch.end_date))
-            sql = "INSERT INTO batches values(default, %s, %s, %s, %s) RETURNING *"
-            cursor = connection.cursor()
-            cursor.executemany(sql, batch_list)
-            connection.commit()
-
-            # ProductCartDAO.delete_cart_items_from_user_id(user_id)
-
-            return "New Batch Added", 200
-        except Exception as e:
-            return "Batch does not exist. Please try again.", 404
 
