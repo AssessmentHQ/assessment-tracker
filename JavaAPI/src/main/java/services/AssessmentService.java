267732f00df5f5ccb90c6a92b5ae4921538f5a2a
Christopher Sei <chrissei@ChristophersMBP.home>
2021-05-28 14:23:16 -0400

    service update

--- JavaAPI/src/main/java/services/AssessmentService.java
+++ JavaAPI/src/main/java/services/AssessmentService.java
@@ -3,8 +3,6 @@ package services;
 import dao.AssessmentDAO;
 import dao.AssessmentDAOImpl;
 import models.Assessment;
-import models.Grade;
-import models.Note;
 import models.Type;
 
 import java.sql.SQLException;
@@ -13,30 +11,27 @@ import java.util.List;
 public class AssessmentService {
     AssessmentDAO assessmentDAO = new AssessmentDAOImpl();
 
+    public List<Assessment> getAssessments() throws SQLException {
+        List<Assessment> assessments = assessmentDAO.getAssessments();
+        return assessments;
+    }
+
+    public List<Assessment> getWeekAssessments(String weekId, int batchId) throws SQLException {
+        List<Assessment> weekAssessments = assessmentDAO.getWeekAssessments(weekId, batchId);
+        return weekAssessments;
+
+    }
+
     public Assessment createAssessment(Assessment assessment) throws SQLException {
         return assessmentDAO.createAssessment(assessment.getWeekId(), assessment.getBatchId());
     }
 
-    public Grade insertGrade(Grade grade) {
-        return null;
-    }
-
-    public Assessment getAssesmentForTrainee(int id, String weekId) {
-        return null;
-    }
-
-    public List<Note> getNotesForTrainee(int id, String weekId) {
-        return null;
-    }
-
-    public int updateWeightForAssessment(int weight) {
-        return 0;
+    public boolean adjustWeight(int assessmentId, int weight) throws SQLException {
+        return assessmentDAO.adjustWeight(assessmentId, weight);
     }
 
     public Type createAssessmentType(Type type) throws SQLException {
         return assessmentDAO.createAssessmentType(type.getName(), type.getDefaultWeight());
     }
-    public int updateTypeForAssessment(int typeId) {
-        return 0;
-    }
+
 }
