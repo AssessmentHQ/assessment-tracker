7faffcf7244d52b7cdca4643bfb15d2100ac15bb
alexjones <akjones1992@yahoo.com>
2021-06-01 16:51:38 -0500

    created default panels for specific_batch_week with some structure functionality

---
+++ web_files/javascript/specific_batch_week.js
@@ -0,0 +1,37 @@
+weekCount = 0
+
+function getWeeks() {
+    let weekTitles = document.getElementsByClassName("week-title")
+    let weekTopics = document.getElementsByClassName("week-topic")
+    let weekDescriptions = document.getElementsByClassName("weekDescription")
+    let weeks = []
+    let count = 1
+    Array.prototype.forEach.call(weekTitles, function (element) {
+        let week = new Week(count, element.value)
+        weeks.push(week)
+        count++
+    });
+    return weeks
+}
+
+function addWeek(){
+    weeks = getWeeks()
+    weekCount += 1
+    document.getElementById("panels").innerHTML += 
+    `
+    <div id="week ${weekCount}" class="col-sm-1-10">
+                    <div class="card mb-5 bg-darker p-3">
+                        <div class="card-body rounded p-3">
+                            <h3 class="card-title"><strong>Week ${weekCount}</strong></h3>
+                            <p class="cart-topic">Outsourcing Your Work and You.</p>
+                            <p class="card-text" class-"weekDescription">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            </p>
+                            <button id="addAssessmentBtn">Add Assessment To This Week</button>
+                            <button id="viewAssessmentsBtn">View Assessments For This Week</button>
+                            <button id="deletePanelBtn">Delete This Week</button>
+                        </div>
+                    </div>
+                </div>
+    `
+}
