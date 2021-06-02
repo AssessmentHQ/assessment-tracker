1bd8972d567f43798f0942fa7d36a735e30f1dac
Zach <50152024+VanVelZ@users.noreply.github.com>
2021-06-02 17:11:47 -0500

    Removed duplicate/old method revived by java merge

--- PythonAPI/daos/batch_dao.py
+++ PythonAPI/daos/batch_dao.py
@@ -8,14 +8,6 @@ class BatchDAO(ABC):
     @abstractmethod
     def get_all_batches_by_year(self, trainer_id, year):
         pass
-    def get_all_batches_by_year(self, year):
-        pass
-
-    def get_all_batches_by_year(self, year: int) -> list[Batch]:
-        pass
-
-    def get_all_batches_by_year(self, year):
-        pass
 
     @abstractmethod
     def get_batch_by_id(self, batch_id):
