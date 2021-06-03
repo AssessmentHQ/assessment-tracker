770d54f81d4b820e091228584706cf0a86186206
alexjones <akjones1992@yahoo.com>
2021-06-03 13:19:01 -0500

    weeks presented are now created based on amount of weeks in batch provided

--- web_files/javascript/specific_batch_week.js
+++ web_files/javascript/specific_batch_week.js
@@ -20,6 +20,15 @@ function setMainNav() {
     </a>`;
 }
 
+let batch = {
+    id: 1,
+    name: "",
+    trainingTrack:"",
+    startDate:"",
+    currentWeek:5,
+    totalWeeks:10
+} 
+
 
 function getWeeks() {
     let weekTitles = document.getElementsByClassName("week-title")
@@ -35,28 +44,132 @@ function getWeeks() {
     return weeks
 }
 
-function addWeek(){
+const panels = document.getElementById("panels");
+
+function addWeek(totalWeeks){
     weeks = getWeeks()
     weekId = document.getElementById("week-title").value
-    weekTopic = document.getElementById("week-topic").value
-    document.getElementById("panels").innerHTML += 
+    for(i = 1; i <= totalWeeks; i++){
+        let gotAssessments = getAssessments(i)
+
+    panels.innerHTML +=
     `
     <div class="d-flex mb-5">
-                        <div id="week_${weekId}" class="col col-11 p-0">
+                        <div id="week_${i}" class="col col-12 p-0">
                             <div class="card bg-darker">
                                 <div class="card-body rounded">
-                                    <h3 class="card-title"><strong>Week ${weekId}</strong></h3>
-                                    <p class="card-topic">${weekTopic}.</p>
-                                    <p class="card-text">
-                                        -No Assessments Yet-
+                                    <h3 class="card-title"><strong>Week ${i}</strong></h3>
+                                    <p class="card-text" id="week${i}Assessments">
+                                        ${gotAssessments ? gotAssessments : "-No Assessments Yet-"}
                                     </p>
                                     <button id="addAssessmentBtn" class="btn btn-primary">Add Assessment To This Week</button>
-                                    <button id="viewAssessmentsBtn" class="btn btn-primary">View Assessments For This Week</button>
-
                                 </div>
                             </div>
                         </div>
-                        <button id="deletePanelBtn" class="btn btn-danger col col-1 del-week align-items-center"><i class="fa fa-trash" aria-hidden="true"></i>Delete Week<br/><i class="fa fa-arrow-left" aria-hidden="true"></i>&nbsp;</button>
+                        
                     </div>
     `
+    }
+}
+addWeek(batch.totalWeeks)
+
+//Get all Current Assessments for a Week
+function getAssessments(weekId) {
+//set the caller_complete to the function that is supposed to receive the response
+let response_func = getAssessments_complete;
+//endpoint: rest api endpoint
+let endpoint = "/batchs/batchId/assessments/weekId"
+//set the url by adding (base_url/java_base_url) + endpoint
+//options:
+//base_url(python)
+//java_base_url(java)
+let url = base_url + endpoint;
+//request_type: type of request
+let request_type = "GET";
+//location you want the response to load
+let response_loc = "loadResult";
+//optional:location you want the load animation to be generated while awaiting the response
+//can be set for any location but will most often be set to response_loc
+//can be left blank if not needed
+let load_loc = response_loc;
+//optional:json data to send to the server
+//can be left blank if not needed
+let jsonData = "";
+console.log(jsonData);
+
+// ajaxgetAssessments(request_type, url, response_func, response_loc, load_loc, jsonData)
+}
+//ajax on-complete function: receives the response from an ajax request
+function getAssessments_complete(status, response, response_loc, load_loc) {
+    //do some logic with the ajax data that was returned
+    //do this if you are expecting a json object - JSON.parse(response)
+
+    //The var "load_loc" is set in case the response message is different from the action to be loaded into the named location
+    //example:
+    //-- you want a message to say "ajax done!" in a popup while the data is compiled and loaded somewhere else
+
+    //action if code 200
+    if(status == 200) {
+        //load the response into the response_loc
+        document.getElementById(response_loc).innerHTML = response;
+        //action if code 201
+    } else if(status == 201) {
+        document.getElementById(response_loc).innerHTML = JSON.parse(response);
+        //action if code 400
+    } else if(status == 400) {
+        //load the response into the response_loc
+        document.getElementById(response_loc).innerHTML = response;
+    }
+}
+
+
+
+
+//Get all Current weeks for a Batch
+function getAllWeeks() {
+    //set the caller_complete to the function that is supposed to receive the response
+    let response_func = getAllWeeks_complete;
+    //endpoint: rest api endpoint
+    let endpoint = "/batchs/batchId/"
+    //set the url by adding (base_url/java_base_url) + endpoint
+    //options:
+    //base_url(python)
+    //java_base_url(java)
+    let url = base_url + endpoint;
+    //request_type: type of request
+    let request_type = "GET";
+    //location you want the response to load
+    let response_loc = "loadResult";
+    //optional:location you want the load animation to be generated while awaiting the response
+    //can be set for any location but will most often be set to response_loc
+    //can be left blank if not needed
+    let load_loc = response_loc;
+    //optional:json data to send to the server
+    //can be left blank if not needed
+    let jsonData = "";
+    console.log(jsonData);
+
+    ajaxgetAllWeeks(request_type, url, response_func, response_loc, load_loc, jsonData)
+}
+//ajax on-complete function: receives the response from an ajax request
+function getAllWeeks_complete(status, response, response_loc, load_loc) {
+    //do some logic with the ajax data that was returned
+    //do this if you are expecting a json object - JSON.parse(response)
+
+    //The var "load_loc" is set in case the response message is different from the action to be loaded into the named location
+    //example:
+    //-- you want a message to say "ajax done!" in a popup while the data is compiled and loaded somewhere else
+
+    //action if code 200
+    if(status == 200) {
+        //load the response into the response_loc
+        document.getElementById(response_loc).innerHTML = response;
+        //action if code 201
+    } else if(status == 201) {
+        document.getElementById(response_loc).innerHTML = JSON.parse(response);
+        //action if code 400
+    } else if(status == 400) {
+        //load the response into the response_loc
+        document.getElementById(response_loc).innerHTML = response;
+    }
 }
