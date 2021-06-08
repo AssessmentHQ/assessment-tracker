9f2d0b3e7e3ba1a5f68ff7a43ed2a5c90edb2f75
Lamar <64709531+lamarhinchen@users.noreply.github.com>
2021-06-08 12:25:14 -0400

    more size fixes for phone

--- PythonAPI/static/javascript/main.js
+++ PythonAPI/static/javascript/main.js
@@ -42,9 +42,11 @@ let mainHeader = `
             </svg>
         </span>
     </a>
-    <a class="navbar-brand order-md-1 order-xs-1 col col-6 col-sm-4 col-md-3 col-lg-2" href="home"><img src="../static/images/rev-logo.png" alt="Revature Logo"></a>
-    <a id="loginBtn" data-toggle="modal" data-target="#loginModal" data-preLoad="Some content to pre-load" class="nav-link text-dark bg-info d-flex order-md-3 order-xs-2 align-items-center mr-3">Log In &nbsp;<i class="fa fa-sign-in" aria-hidden="true"></i></a>
-    <div class="collapse navbar-collapse order-md-2 order-xs-3 col-xs-12 bg-dark py-3" id="navbarToggle">
+    <a class="navbar-brand order-xs-1 order-sm-1 order-md-1 order-lg-1 order-md-1 col col-6 col-sm-4 col-md-3 col-lg-2" href="home"><img src="../static/images/rev-logo.png" alt="Revature Logo"></a>
+    <div class="order-xs-3 order-sm-3 order-md-2 order-lg-3 order-md-3 mr-3 col col-3 col-sm-2">
+        <a id="loginBtn" data-toggle="modal" data-target="#loginModal" data-preLoad="Some content to pre-load" class="nav-link d-flex text-dark bg-info align-items-center">Log In &nbsp;<i class="fa fa-sign-in" aria-hidden="true"></i></a>
+    </div>
+    <div class="collapse navbar-collapse order-xs-3 order-sm-3 order-md-3 order-lg-2 col-xs-12 bg-dark py-3" id="navbarToggle">
         <form class="mr-5 col-md-9 col-sm-12 needs-validation" novalidate autocomplete="off">
             <div class="input-group">
                 <div class="input-group-prepend">
